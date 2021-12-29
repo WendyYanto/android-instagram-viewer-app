164a45c65ccde322cb1505098fb157ba3a4b993d
wendyyanto <yanto.wendy98@gmail.com>
2021-12-29 19:26:23 +0700

    setup entry point

--- app/src/main/java/dev/wendyyanto/instagramviewerapp/main/MainActivity.kt
+++ app/src/main/java/dev/wendyyanto/instagramviewerapp/main/MainActivity.kt
@@ -1,10 +1,12 @@
 package dev.wendyyanto.instagramviewerapp.main
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import dev.wendyyanto.instagramviewerapp.R
+import dev.wendyyanto.instagramviewerapp.gallery.GalleryActivity
 import dev.wendyyanto.instagramviewerapp.instagrampermission.InstagramAuthenticationWebViewActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +17,30 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    textView = findViewById(R.id.tv_login_to_instagram)
+    textView = findViewById(R.id.tv_instagram_gallery)
 
     textView.setOnClickListener {
-      goToInstagramPopupWindowWebView()
+      if (hasInstagramAccessToken()) {
+        goToGallery()
+      } else {
+        goToInstagramPermissionPage()
+      }
     }
   }
 
-  private fun goToInstagramPopupWindowWebView() {
+  private fun hasInstagramAccessToken(): Boolean {
+    val token = getSharedPreferences("instagram", Context.MODE_PRIVATE)
+      .getString("ACCESS_TOKEN", "")
+      .orEmpty()
+    return token.isNotBlank()
+  }
+
+  private fun goToGallery() {
+    val intent = Intent(this, GalleryActivity::class.java)
+    startActivity(intent)
+  }
+
+  private fun goToInstagramPermissionPage() {
     val intent = Intent(this, InstagramAuthenticationWebViewActivity::class.java)
     startActivity(intent)
   }
